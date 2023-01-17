a4eceef9f99e0d98f9b8032a3c3dbef249e90bc6
Krishna Manchikanti <krishnamanchikanti@192.168.1.2>
2023-01-17 10:40:19 +0530

    created routes & controllers separately to make project scalable

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+
+module.exports.home = (req,res)=>{
+    res.end('<h1>Hey Welcome</h1>');
+}
---
+++ controllers/users_controller.js
@@ -0,0 +1,4 @@
+
+module.exports.profile = (req,res)=>{
+     res.end('<h1> Hey Its users Profile</h1>');
+}
