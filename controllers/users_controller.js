cd64cdda5ee0498d460ee2cbc04a1553b42bfcd3
Krishna Manchikanti <krishnamanchikanti@192.168.1.4>
2023-01-20 18:17:36 +0530

    Created SIgn-in & Sign-up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,22 @@ module.exports.profile = (req,res)=>{
         title:"User"
     });
 }
+
+module.exports.signIn = (req,res)=>{
+    return res.render('users_sign_in',{
+        title:"signIn"
+    });
+}
+module.exports.signUp = (req,res)=>{
+    return res.render('users_sign_up',{
+        title:"signIn"
+    });
+}
+
+module.exports.create = (req,res)=>{
+
+}
+
+module.exports.createSession = (req,res)=>{
+    
+}
