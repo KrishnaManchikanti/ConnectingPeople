aefe9533c511fb39a9aacd9e58be564328d342c0
Krishna Manchikanti <krishnamanchikanti@192.168.1.9>
2023-07-01 15:20:31 +0530

    deletingPost Autheticated && removing comments related to post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
-
+const Comments = require('../models/comment');
+const { localsName } = require('ejs');
 module.exports.create = (req,res)=>{
 
     Post.create({
@@ -11,4 +12,19 @@ module.exports.create = (req,res)=>{
         return res.redirect('back');
     });
     
+}
+
+module.exports.destroy = (req,res)=>{
+
+    Post.findById(req.params.id,(err,post)=>{
+        if(post.user == req.user.id){
+            post.remove();//removing whole collection in db(mongoose)
+            Comments.deleteMany({post:req.params.id},(err)=>{
+                if(err){console.log(`err in deleting comment ${post._id}`)};
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,8 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postsController.create);
+//using params we are sending id of the post 
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
+
 
 module.exports = router;
