3065d53cf2aed03c0e1b0cd9f7f0d9f227397173
Krishna Manchikanti <krishnamanchikanti@192.168.1.10>
2023-07-12 20:18:16 +0530

    Deleting Comments using $pull from an array

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -12,10 +12,30 @@ module.exports.create = (req,res)=>{
                 if(err){console.log("err in creating comment",err);return;}
                 post.comments.push(comment);
                 post.save();
-
                 res.redirect('/');
             });
         }
     });
-   
+}
+
+module.exports.destroy = (req,res)=>{
+    
+    Comment.findById(req.params.id,(err,comment)=>{
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            comment.remove();
+                
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}},(err,post)=>{
+                if(err){console.log("error in find post")};
+                return res.redirect('back');
+            });
+// Post.findById(postId,(err,post)=>{
+//     if(err){console.log("error in find post")};
+//     post.comments.pop(req.params.id);
+//     post.save();
+// }) 
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
